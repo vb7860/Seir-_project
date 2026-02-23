31638ed9d8eb56f9614c6f7b2a0d1f377e3b74f2
vb7860 <vishalbhardwaj786049@gmail.com>
2026-02-23 22:06:47 +0530

    Update project with new version

--- project2.py
+++ project2.py
@@ -1,8 +1,6 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-import re
-from collections import Counter
 def main():
     if len(sys.argv) != 2:
         print("put correct url")
@@ -21,33 +19,7 @@ def main():
                 address = link.get('href')
                 if address:
                     print(address)
-            word_counts = get_word_frequencies(body_text)
-            for word, count in word_counts.items():
-                h_val = polynomial_hash(word)
-                print(f"Word: {word} | Count: {count} | Hash: {h_val}")
     else:
         print("i Can not fetch page")
-        sys.exit()
-def get_word_frequencies(text):
-    words = re.findall(r'\w+', text.lower())
-    word_counts = {}
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-
-    return word_counts
-def polynomial_hash(word):
-    p = 53
-    m = 2**64
-    h = 0
-    power = 1
-    for char in word:
-        h = (h + ord(char) * power) % m
-        power = (power * p) % m
-    return h
-
-
-    
+        sys.exit()  
 main()
